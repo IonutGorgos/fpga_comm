b982b9fab49c8b65ca128f6a7ebe729802342daf
IonutGorgos <gorgosionut@yahoo.com>
2016-06-07 16:45:05 +0300

    Number_traces

---
+++ Ciphertext.py
@@ -0,0 +1,40 @@
+import sasebo_ftdi
+import binascii
+from Crypto import Random
+from Crypto.Cipher import AES
+
+hw = sasebo_ftdi.SASEBO()
+hw.open()
+rand = Random.new()
+
+#key = bytearray(rand.getrandbits(8) for _ in xrange(16))
+key = rand.read(16)
+#print "Key                   : " , binascii.hexlify(key).upper()
+
+#Initialization
+
+hw.setKey(key,16)       # Hardware setKey
+sw = AES.new(key, AES.MODE_ECB) # Software SetKey
+
+num_trace = 1000
+i = 1
+while i <= num_trace:
+    progress = (100.0 * i / num_trace)
+    print
+    print "Trace nr. : ", i, "         Progress : ", progress,"%"
+    text_in = rand.read(16)
+    print "Plain text            : ", binascii.hexlify(text_in).upper()
+
+    text_ans = sw.encrypt(text_in)      # Ciphertext from Crypto.AES
+    print "Cipher text(Software) : ", binascii.hexlify(text_ans).upper()
+
+    text_out = bytearray(16)
+
+    hw.writeText(text_in, 16)
+    hw.execute()
+    bytes = hw.readText(text_out, 16)       # Ciphertext from SASEBO
+    print "Cipher text(Hardware) : ",  binascii.hexlify(bytes).upper()
+
+    i = i+1
+print "Key                   : " , binascii.hexlify(key).upper()
+hw.close()
