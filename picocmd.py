822c0946af75a9a6e409d9a6db58ec9534fdb7fa
IonutGorgos <gorgosionut@yahoo.com>
2016-06-14 22:25:01 +0300

    Pico command line and text file

--- picocmd.py
+++ picocmd.py
@@ -1,14 +1,49 @@
 import pico
 
+file = open("conf1.txt", "r")
+data = file.readlines()
+file.close()
+duration = float(data[0])
+sampleInterval = float(data[1])
+trigger = data[2].split('\n')
+trigger = trigger[0]
+threshold = float(data[3])
+direction = data[4].split('\n')
+direction = direction[0]
+timeout = int(data[5])
+enabledTrig = bool(data[6])
+nr_traces = int(data[7])
+pre_trig = float(data[8])
+downValues = int(data[9])
+downMode = int(data[10])
+filename = data[11].split('\n')
+filename = filename[0]
+ch1 = data[12].split('\n')
+ch2 = data[13].split('\n')
+ch3 = data[14].split('\n')
+ch1 = ch1[0]
+ch2 = ch2[0]
+ch3 = ch3[0]
+coupling = data[15].split('\n')
+coupling = coupling[0]
+vR1 = float(data[16])
+vR2 = float(data[17])
+vR3 = float(data[18])
+enabled1 = bool(data[19])
+enabled2 = bool(data[20])
+enabled3 = bool(data[21])
+BWL1 = int(data[22])
+BWL2 = int(data[23])
+BWL3 = int(data[24])
 ps = pico.PICO()
 ps.open()
 print("Attempting to open Picoscope 6000...")
-ps.parameters(2 * 1E-6, 3.2 * 1E-9)
-# ps.get_samples(2*1E-6,3.2*1E-9)
+ps.parameters(duration, sampleInterval)
 ps.get_samples()
-ps.trigger('B', 0.0, 'Falling', 100, True)
-ps.arm(10, 0.5, 16, 2, 'test_', 'A', 'B', chNum3='C', chCoupling='DC',
-       chVRange1=50E-3, chVRange2=500E-3, chVRange3=50E-3, enabled1=True,
-       enabled2=True, enabled3=False, BWLimited1=2, BWLimited2=0, BWLimited3=0)
+ps.trigger(trigger, threshold, direction, timeout, enabledTrig)
+ps.arm(nr_traces, pre_trig, downValues, downMode, filename, ch1, ch2, ch3,
+       coupling,
+       vR1, vR2, vR3, enabled1,
+       enabled2, enabled3, BWL1, BWL2, BWL3)
 print("Attempting to close Picoscope 6000...")
 ps.close()
